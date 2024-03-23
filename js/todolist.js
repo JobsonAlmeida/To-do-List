6b02bf8c874ad57908968e8e795e2a6af897bb4e
Jobson Almeida <jobsonbox@gmail.com>
2024-03-23 12:37:17 -0300

    Created addTask function

--- js/todolist.js
+++ js/todolist.js
@@ -6,22 +6,51 @@
     const itemInput = document.getElementById("item-input")
     const todoAddForm = document.getElementById("todo-add")
     const ul = document.getElementById("todo-list")
+    const lis = ul.getElementsByTagName("li")
 
+
+
+    function addTask(task){
+        const li = document.createElement("li")        
+        li.className = "todo-item"
+
+        const p = document.createElement("p")
+        p.className = "task-name"
+        p.textContent= task
+
+        li.appendChild(p)
+
+        ul.appendChild(li)
+
+        li.addEventListener("click",  function(e){
+            console.log(this)
+            
+        })
+
+    }
 
     todoAddForm.addEventListener("submit", function(e){
         e.preventDefault()
 
-
         console.log(itemInput.value)
-        ul.innerHTML += `
-            <li class="todo-item">        
-                <p class="task-name">${itemInput.value}</p>
-            </li>
-        `
+        // ul.innerHTML += `
+        //     <li class="todo-item">        
+        //         <p class="task-name">${itemInput.value}</p>
+        //     </li>
+        // `
 
+        addTask(itemInput.value)
 
         itemInput.value = ""
 
-        itemInput.focus()
-    })
+        itemInput.focus(itemInput.valu)
+
+    });
+
+    [...lis].forEach(element => {
+        element.addEventListener("click", function(e){
+            console.log(this)
+        })
+    });
+
 })()

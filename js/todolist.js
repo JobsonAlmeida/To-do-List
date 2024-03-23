da3a669277db4d22c5d0c76df73514fbab21f02d
Jobson Almeida <jobsonbox@gmail.com>
2024-03-23 13:51:40 -0300

    Created arrTasks array and restructured the project to render each one of the tasks from this array into the screen

--- js/todolist.js
+++ js/todolist.js
@@ -6,8 +6,16 @@
     const itemInput = document.getElementById("item-input")
     const todoAddForm = document.getElementById("todo-add")
     const ul = document.getElementById("todo-list")
-    const lis = ul.getElementsByTagName("li")
+    // const lis = ul.getElementsByTagName("li")
 
+    let arrTasks = [
+        {
+            name: "task 1",
+            createdAt: Date.now(),
+            completed: false
+
+        }
+    ]
 
     function addEventLi(li){
 
@@ -18,22 +26,42 @@
 
     }
 
-    function addTask(task){
+    function generateLiTask(obj){
+
         const li = document.createElement("li")        
         li.className = "todo-item"
 
         const p = document.createElement("p")
         p.className = "task-name"
-        p.textContent= task
+        p.textContent= obj.name
 
         li.appendChild(p)
 
-        ul.appendChild(li)
-
         addEventLi(li)
 
-      
+        return li
+   
+    }
 
+    function renderTasks(){
+
+        ul.innerHTML = ""
+
+        arrTasks.forEach(taskObj => {
+            ul.appendChild(generateLiTask(taskObj))
+        });
+    }
+
+
+    function addTask(task){
+
+        arrTasks.push({
+            name: task,
+            createdAt: Date.now(),
+            completed: false
+
+        })
+      
     }
 
     todoAddForm.addEventListener("submit", function(e){
@@ -48,14 +76,15 @@
 
         addTask(itemInput.value)
 
+        renderTasks()
+
         itemInput.value = ""
 
-        itemInput.focus(itemInput.valu)
+        itemInput.focus(itemInput)
 
     });
 
-    [...lis].forEach(li => {
-        addEventLi(li)
-    });
+    
+    renderTasks()
 
 })()
